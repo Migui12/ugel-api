9965cfbb3cfdca6c45427350a4ff140a0c74c075
Migui12 <134223160+Migui12@users.noreply.github.com>
2026-02-19 14:22:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(helmet({
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://ugel-api.onrender.com/api',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
